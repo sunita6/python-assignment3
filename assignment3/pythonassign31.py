2f77908ab8348ae1422608a3b885e09ada12a77d
sunita6 <68330255+sunita6@users.noreply.github.com>
2020-07-23 11:13:30 +0530

    Update pythonassign31.py

--- assignment3/pythonassign31.py
+++ assignment3/pythonassign31.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 n=int(input("enter the number:"))
 sum1=0
 while(n>0):
@@ -11,8 +5,6 @@ while(n>0):
     n=n-1
 print("the sum of n natural number is",sum1)
 
-
-# In[ ]:
 
 
 
